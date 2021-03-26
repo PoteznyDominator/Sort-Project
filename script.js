b16d5dc977db3965f62ccb70651cc4d88901ee4a
poteznydominator <b.murawski00@gmail.com>
2021-03-26 21:10:09 +0100

    Adding first Sort algorithm and making it pretty.

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 const element = document.querySelectorAll("#bubble .element")
 const container = document.querySelector(".sort-container");
+const startBtn = document.querySelector(".start");
 
-let tab = [];
+const timer = ms => new Promise(res => setTimeout(res, ms))
 
+startBtn.addEventListener("click",function(){
+    container.style.height = "270px";
+    bubbleSort();
+});
 
-
-let i = 0;
-function myLoop(){
-    setTimeout(function(){
-        if(i<element.length){
-            tab[i] = parseInt(element[i].getElementsByTagName("p")[0].innerHTML);
-            console.log(tab)
-            myLoop();
-            let animation = anime({
-                targets:element[i],
-                translateY:100,
-            });
-        }
-        else{
-            let animation = anime({
-            targets:element,
-            translateY:0,
-            });
-            i = -1;
-            myLoop();
-        }
-        i++;
-    },1000);
-}
-myLoop();
-
-
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
+let bubbleSort = async () => {
+    let tab = Array.from(element);
+    let len = tab.length;
+    await timer(500);
     for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j] > inputArr[j + 1]) {
-                let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
-            }
+        for (let j = 0; j < len-1-i; j++) {
+                anime({
+                    targets:tab[j],
+                    translateY:100,
+                })
+                anime({
+                    targets:tab[j+1],
+                    translateY:100,
+                })
+                if (parseInt(tab[j].getElementsByTagName("p")[0].innerHTML)>parseInt(tab[j+1].getElementsByTagName("p")[0].innerHTML)) {
+                    await timer(1000);
+                    anime({
+                        targets:tab[j],
+                        translateX: {
+                            value:'+=76',
+                        },
+                    })
+                    anime({
+                        targets:tab[j+1],
+                        translateX: {
+                            value:'-=76',
+                        },
+                    })
+                    let tmp = tab[j];
+                    tab[j] = tab[j+1];
+                    tab[j+1] = tmp;
+                }
+                await timer(1000);
+                anime({
+                    targets:tab[j],
+                    translateY:0,
+                })
+                anime({
+                    targets:tab[j+1],
+                    translateY:0,
+                })
+                await timer(1000);
         }
     }
-    return inputArr;
+    return tab;
 };
-bubbleSort(tab);
-console.log(tab);
